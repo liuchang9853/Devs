248a1900dd6cdd1a6418bc7e5e8cf762f4bd51ee
Devs <64179190+DeusOfDeepThoughts@users.noreply.github.com>
2020-05-07 09:48:34 +0800

    Create 二分查找.cpp

---
+++ C++基本算法/二分查找.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int binSearch(const int *Array,int start,int end,int key) {
+	int left,right;
+	int mid;
+	left=start;
+	right=end;
+	while(left<=right){
+		mid=left+(right-left)/2;
+		if(key==Array[mid])  return mid;
+		else if(key<Array[mid]) right=mid-1;
+		else if(key>Array[mid]) left=mid+1;
+	}
+	return -1;
+}
+int main() {
+	int array [] = {1,3,5,6,7,9,100,111,200};
+	cout << binSearch(array,0,8,111);
+	return 0;
+}
