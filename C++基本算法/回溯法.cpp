52e6cb5335d25ea6553270301c90a6724fee2876
Devs <64179190+DeusOfDeepThoughts@users.noreply.github.com>
2020-05-12 15:04:49 +0800

    Update 回溯法.cpp

--- C++基本算法/回溯法.cpp
+++ C++基本算法/回溯法.cpp
@@ -29,90 +29,125 @@ struct MatrixElem {
 struct Matrix {
 	int rowNum = 3;
 	int colNum = 4;
-	MatrixElem matrix[3][4];
+	MatrixElem matrix[3][4] = { {},{},{} };
 };
 
 struct String {
-	const char * str;
+	const char* str;
 	int length;
 	int strPtr;
 };
+/*
+1.最重要的经验就是上面这三个struct，即要养成 封装对象 的习惯。
+MatrixElem 和 Matrix，最开始没有后者，但path()函数中就要引入具体的数值3和4，
+泛化能力弱，而且也不符合对象理解的习惯。
+若没有String，则在传char *参数时，就要同时传递str和strLen，参数列表复杂，不仅
+易出错，且属于同一对象的属性被分别考虑。
+能写成class、struct、enum、union等具有封装意义的形式，就一定要写成这样的形式。
 
-bool ifUnvisited(stack<MatrixElem> &stepStack , MatrixElem elem) {
+2.能不定义全局变量，就不定义。比如，写成：
+struct Matrix{
+... 
+} m; 
+远不及在需要m的地方定义m好，只把Matrix这一struct类的定义放到全局空间就行。
+
+3.Matrix中成员具有初值。 
+*/
+
+bool ifUnvisited(stack<MatrixElem>& stepStack, MatrixElem elem) {
 	int staLen = int(stepStack.size());
 	stack<MatrixElem> tempStack;
-	for(int i = 0 ; i < staLen ; ++i) {
+	for (int i = 0; i < staLen; ++i) {
 		tempStack.push(stepStack.top());
 		stepStack.pop();
 	}
-	for(int i = 0 ; i < staLen ; ++i) {
+	for (int i = 0; i < staLen; ++i) {
 		stepStack.push(tempStack.top());
 		tempStack.pop();
-		if(stepStack.top().data == elem.data ) return false;
+		if (stepStack.top().data == elem.data) return false;
 	}
 	return true;
 }
 
 //核心
-void path(Matrix m , MatrixElem elem , String str , stack<MatrixElem> &stepStack) {
-	if(str.strPtr >= str.length) {
+void path(Matrix m, MatrixElem elem, String &str, stack<MatrixElem>& stepStack) {
+	if (str.strPtr >= str.length-1) {
 		int staLen = int(stepStack.size());
 		stack<MatrixElem> tempStack;
-		for(int i = 0 ; i < staLen ; ++i) {
+		for (int i = 0; i < staLen; ++i) {
 			tempStack.push(stepStack.top());
 			stepStack.pop();
 		}
-		for(int i = 0 ; i < staLen ; ++i) {
-			cout << tempStack.top().data << "(" << tempStack.top().row << "," << tempStack.top().col << ")" << ends;
+		for (int i = 0; i < staLen; ++i) {
 			stepStack.push(tempStack.top());
 			tempStack.pop();
+			cout << stepStack.top().data << "(" << stepStack.top().row << "," << stepStack.top().col << ")" << ends;
 		}
 
 		cout << endl;
-		return ;
+		return;
 	}
 
-	bool top = true , left = true , bottom = true , right = true;
-	if(elem.row == 0) top = false;
-	if(elem.row == m.rowNum-1) bottom = false;
-	if(elem.col == 0) left = false;
-	if(elem.col == m.colNum-1) right = false;
+	bool top = true, left = true, bottom = true, right = true;
+	if (elem.row == 0) top = false;
+	if (elem.row == m.rowNum - 1) bottom = false;
+	if (elem.col == 0) left = false;
+	if (elem.col == m.colNum - 1) right = false;
 
-
-	if(top)
-		if(m.matrix[elem.row-1][elem.col].data == str.str[++str.strPtr] && ifUnvisited(stepStack , m.matrix[elem.row-1][elem.col])) {
-			stepStack.push(m.matrix[elem.row-1][elem.col]);
-			path(m , m.matrix[elem.row-1][elem.col] , str , stepStack);
+	if (top)
+		if (str.strPtr < str.length && m.matrix[elem.row - 1][elem.col].data == str.str[str.strPtr + 1] && ifUnvisited(stepStack, m.matrix[elem.row - 1][elem.col])) {
+			stepStack.push(m.matrix[elem.row - 1][elem.col]);
+			++str.strPtr;
+			path(m, m.matrix[elem.row - 1][elem.col], str, stepStack);
+			--str.strPtr;
 			stepStack.pop();
 		}
-	if(left)
-		if(m.matrix[elem.row][elem.col-1].data == str.str[++str.strPtr] && ifUnvisited(stepStack , m.matrix[elem.row][elem.col-1])) {
-			stepStack.push(m.matrix[elem.row][elem.col-1]);
-			path(m , m.matrix[elem.row][elem.col-1] , str , stepStack);
+	if (left)
+		if (str.strPtr < str.length && m.matrix[elem.row][elem.col - 1].data == str.str[str.strPtr + 1] && ifUnvisited(stepStack, m.matrix[elem.row][elem.col - 1])) {
+			stepStack.push(m.matrix[elem.row][elem.col - 1]);
+			++str.strPtr;
+			path(m, m.matrix[elem.row][elem.col - 1], str, stepStack);
+			--str.strPtr;
 			stepStack.pop();
 		}
-	if(bottom)
-		if(m.matrix[elem.row+1][elem.col].data == str.str[++str.strPtr] && ifUnvisited(stepStack , m.matrix[elem.row+1][elem.col])) {
-			stepStack.push(m.matrix[elem.row+1][elem.col]);
-			path(m , m.matrix[elem.row+1][elem.col] , str , stepStack);
+	if (bottom)
+		if (str.strPtr < str.length && m.matrix[elem.row + 1][elem.col].data == str.str[str.strPtr + 1] && ifUnvisited(stepStack, m.matrix[elem.row + 1][elem.col])) {
+			stepStack.push(m.matrix[elem.row + 1][elem.col]);
+			++str.strPtr;
+			path(m, m.matrix[elem.row + 1][elem.col], str, stepStack);
+			--str.strPtr;
 			stepStack.pop();
 		}
-	if(right)
-		if(m.matrix[elem.row][elem.col+1].data == str.str[++str.strPtr] && ifUnvisited(stepStack , m.matrix[elem.row][elem.col+1])) {
-			stepStack.push(m.matrix[elem.row][elem.col+1]);
-			path(m , m.matrix[elem.row][elem.col+1] , str , stepStack);
+	if (right)
+		if (str.strPtr < str.length && m.matrix[elem.row][elem.col + 1].data == str.str[str.strPtr + 1] && ifUnvisited(stepStack, m.matrix[elem.row][elem.col + 1])) {
+			stepStack.push(m.matrix[elem.row][elem.col + 1]);
+			++str.strPtr;
+			path(m, m.matrix[elem.row][elem.col + 1], str, stepStack);
+			--str.strPtr;
 			stepStack.pop();
+			/*
+			注意在递归中，这样的形式：
+			stack.push();
+			++ptr;
+			递归函数(stack,ptr);
+			--ptr 
+			stack.pop();
+			即递归函数需要的参数，若为进入递归函数而改变，则递归函数结束后，
+			应恢复。 
+			*/ 
 		}
 }
 
 //启动函数
-void pathTraverse(Matrix m , String str) {
+void pathTraverse(Matrix m, String &str) {
 	stack<MatrixElem> stepStack;
-	for(int i=0; i<3; ++i)
-		for(int j=0; j<4; ++j)
-			if(m.matrix[i][j].data == str.str[0]) {
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 4; ++j)
+			if (m.matrix[i][j].data == str.str[0]) {
 				stepStack.push(m.matrix[i][j]);
-				path(m , m.matrix[i][j] , str , stepStack);
+				++str.strPtr;
+				path(m, m.matrix[i][j], str, stepStack);
+				--str.strPtr;
 				stepStack.pop();
 			}
 }
@@ -120,9 +155,9 @@ void pathTraverse(Matrix m , String str) {
 int main() {
 	//初始化m
 	Matrix m;
-	char arr[3][4] = {{'a','b','t','g'},{'c','f','c','s'},{'j','d','e','h'}};
-	for(int i=0; i<3; ++i)
-		for(int j=0; j<4; ++j) {
+	char arr[3][4] = { {'a','b','t','g'},{'c','f','c','s'},{'j','d','e','h'} };
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 4; ++j) {
 			m.matrix[i][j].row = i;
 			m.matrix[i][j].col = j;
 			m.matrix[i][j].data = arr[i][j];
@@ -133,13 +168,15 @@ int main() {
 	str_1.str = "bfce";
 	str_1.length = 4;
 	str_1.strPtr = -1;
-	pathTraverse(m , str_1);
+	cout << "the path of str_1 : "; 
+	pathTraverse(m, str_1);
 
 	String str_2;
-	str_2.str = "abfd";
+	str_2.str = "abfb";
 	str_2.length = 4;
 	str_2.strPtr = -1;
-	pathTraverse(m , str_2);
+	cout << "the path of str_2 : "; 
+	pathTraverse(m, str_2);
 
 	return 0;
 }
