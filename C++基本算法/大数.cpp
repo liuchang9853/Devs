a5e651d12233d0a6bf55132cce5bc420e87be329
Devs <64179190+DeusOfDeepThoughts@users.noreply.github.com>
2020-05-14 10:45:12 +0800

    Create 大数.cpp

---
+++ C++基本算法/大数.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+struct bigData {
+	int * numArr;
+	int numLen;
+	int index;
+};
+void with_carry(bigData &data , int carry) {
+	data.numArr[data.index] += carry;
+	if(data.numArr[data.index] == 10) {
+		data.numArr[data.index] = 0;
+		
+		data.index -= 1;
+		with_carry(data , 1);
+		data.index += 1;
+	}
+}
+void print_1_to_max(int n) {
+	//大数初始化
+	bigData data;
+	data.numArr = new int[n + 1](); //numArr[0]用于存放最高位进位以判断是否输出完毕。这是哨兵。 
+	data.numLen = n;
+	data.index = n;
+
+	//大数加法
+	for(int i = 0 ; i < 10 && data.numArr[0] == 0 ; ++i) {
+		//i：0~9 对应 data.numArr[data.index]：1~10 
+		++data.numArr[data.index];
+
+		//有进位，先进位
+		if(data.numArr[data.index] == 10) {
+			data.numArr[data.index] = 0;
+
+			//递归特色夹心饼干
+			data.index -= 1;
+			with_carry(data , 1);
+			data.index += 1;
+			
+			//每进完一次位，要置for循环重新开始。 
+			i = -1;
+		}
+
+		//打印
+		if(data.numArr[0] == 0) {
+			int j = 0;
+			for( ; data.numArr[j] == 0 ; ++j);
+			for( ; j <= data.numLen ; ++j) cout << data.numArr[j];
+			cout << ends;
+		}
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+	print_1_to_max(n);
+	return 0;
+}
