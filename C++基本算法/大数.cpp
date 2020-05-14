4d96ce8cb06768874a03b6e45d98dd203dea3b91
Devs <64179190+DeusOfDeepThoughts@users.noreply.github.com>
2020-05-14 10:57:09 +0800

    Update 大数.cpp

--- C++基本算法/大数.cpp
+++ C++基本算法/大数.cpp
@@ -1,3 +1,7 @@
+/*
+题目：输入数字n，按顺序打印出从1最大的n位十进制数。比如输入3，则
+打印出1、2、3、4、5一直到最大的3位数即999。
+*/ 
 #include <iostream>
 using namespace std;
 struct bigData {
@@ -22,7 +26,7 @@ void print_1_to_max(int n) {
 	data.numLen = n;
 	data.index = n;
 
-	//大数加法
+	//大数按序输出 
 	for(int i = 0 ; i < 10 && data.numArr[0] == 0 ; ++i) {
 		//i：0~9 对应 data.numArr[data.index]：1~10 
 		++data.numArr[data.index];
